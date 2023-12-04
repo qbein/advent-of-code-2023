59dc77534d921d15e67bf727b4ab518398211163
Kurt Aadnøy <kurt@aadnoy.info>
2023-12-04 23:15:49 +0100

    Solved day 3 part 2

--- AdventOfCode2023/Day03.cs
+++ AdventOfCode2023/Day03.cs
@@ -4,7 +4,9 @@ namespace AdventOfCode2023;
 
 public class Day03
 {
-    private static Regex PartNumberRegex = new("[0-9]+");
+    private static readonly Regex PartNumberRegex = new("[0-9]+");
+
+    private const char Gear = '*';
     
     public static long SolvePart01(string[] schematic)
     {
@@ -26,6 +28,80 @@ public class Day03
         }
 
         return count;
+    }
+
+    public static long SolvePart02(string[] schematic)
+    {
+        long gearRatioSum = 0;
+
+        for (var lineIndex = 0; lineIndex < schematic.Length; lineIndex++)
+        {
+            for (var colIndex = 0; colIndex < schematic[lineIndex].Length; colIndex++)
+            {
+                if (schematic[lineIndex][colIndex] != Gear) continue;
+
+                var adjacentInts = GetAdjacentInts(schematic, lineIndex, colIndex);
+                if (adjacentInts.Count == 2)
+                    gearRatioSum += adjacentInts.Aggregate((current, next) => current * next);
+            }
+        }
+
+        return gearRatioSum;
+    }
+
+    private static IList<int> GetAdjacentInts(
+        string[] schematic,
+        int lineIndex,
+        int colIndex
+    )
+    {
+        var ints = new List<int>();
+        
+        // Row above
+        if (lineIndex > 0)
+            ints.AddRange(GetLineInts(schematic[lineIndex-1], colIndex, false));
+        
+        // Adjacent
+        ints.AddRange(GetLineInts(schematic[lineIndex], colIndex, true));
+        
+        // Row below
+        if (lineIndex < schematic.Length)
+            ints.AddRange(GetLineInts(schematic[lineIndex+1], colIndex, false));
+        
+        return ints;
+    }
+
+    private static IList<int> GetLineInts(string line, int colIndex, bool onlyAdjacent)
+    {
+        var ints = new List<int>();
+        
+        if (!onlyAdjacent && char.IsDigit(line[colIndex]))
+            ints.Add(ExtractInt(line, colIndex));
+        else
+        {
+            if (colIndex > 0 && char.IsDigit(line[colIndex - 1]))
+                ints.Add(ExtractInt(line, colIndex - 1));
+            if (colIndex < line.Length && char.IsDigit(line[colIndex + 1]))
+                ints.Add(ExtractInt(line, colIndex + 1));
+        }
+
+        return ints;
+    }
+
+    private static int ExtractInt(string line, int colIndex)
+    {
+        if (!char.IsDigit(line[colIndex]))
+            throw new InvalidOperationException($"Character ({line[colIndex]}) is not a digit");
+
+        var startIndex = colIndex;
+        while (startIndex > 0 && char.IsDigit(line[startIndex - 1]))
+            startIndex--;
+
+        var endIndex = colIndex;
+        while (endIndex < line.Length && char.IsDigit(line[endIndex]))
+            endIndex++;
+
+        return int.Parse(line.Substring(startIndex, endIndex - startIndex));
     }
 
     private static bool HasAdjacentSymbol(
--- AdventOfCode2023Tests/Day03Test.cs
+++ AdventOfCode2023Tests/Day03Test.cs
@@ -25,4 +25,26 @@ public class Day03Test
             )
         );
     }
+    
+    [Fact]
+    public async Task Day0302_sample_solve()
+    {
+        Assert.Equal(
+            467835,
+            Day03.SolvePart02(
+                await File.ReadAllLinesAsync("Files/Day0301-sample.txt")
+            )
+        );
+    }
+    
+    [Fact]
+    public async Task Day0302_solve()
+    {
+        Assert.Equal(
+            81463996,
+            Day03.SolvePart02(
+                await File.ReadAllLinesAsync("Files/Day0301.txt")
+            )
+        );
+    }
 }
